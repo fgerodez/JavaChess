e63deaa04c2aabe8d07b34177de4c77bade6fd25
Ouzned <ouzned.alleykatze@gmail.com>
2013-05-24 22:57:41 +0200

    refactoring

--- src/com/javachess/joueurs/Joueur.java
+++
@@ -1,9 +0,0 @@
-package com.javachess.joueurs;
-
-import com.javachess.helpers.Color;
-import com.javachess.helpers.Move;
-
-public interface Joueur {
-	public Move jouer();
-	public Color getCouleur();
-}
